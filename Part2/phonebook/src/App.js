9cbd665345c863af33ed58263f18017fffd0fba1
askming <askming@gmail.com>
2020-08-16 22:59:08 -0700

    phonebook  step11

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -4,12 +4,14 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 // import axios from 'axios'
 import personService from './services/persons'
+import './index.css'
 
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchName, setSearchName ] = useState('')
+  const [ successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -20,6 +22,18 @@ const App = () => {
       })
   }, [])
   // console.log('render', persons.length, 'persons')
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+
+    return(
+      <div className = "success">
+        {message}
+      </div>
+    )
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -39,9 +53,18 @@ const App = () => {
         .update(changedPerson)
         .then(retunedPerons =>{
           setPersons(persons.map(person => person.name !== personObject.name ? person : personObject))
-          setNewName('')
-          setNewNumber('')
         })
+        .finally(success => {
+          setSuccessMessage(
+            `Updated ${personObject.name}`
+          )
+          setTimeout(() =>{
+            setSuccessMessage(null)
+          }, 5000)
+        })
+        console.log(successMessage)
+        setNewName('')
+        setNewNumber('')
         }
       }
 
@@ -50,11 +73,18 @@ const App = () => {
       .create(personObject)
       .then(returnedObject =>{
         setPersons(persons.concat(returnedObject))
+      })
+      .finally(success => {
+        setSuccessMessage(
+          `Added ${personObject.name}`
+        )
+        setTimeout(() =>{
+          setSuccessMessage(null)
+        }, 5000)
         setNewName('')
         setNewNumber('')
       })
     }
-    
   }
 
   const removeHandler = (id) =>{
@@ -97,6 +127,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message = {successMessage} />
         <Filter value = {searchName} handler = {handleSearchPerson} />
       <h3>add a new</h3>
         <PersonForm addPerson = {addPerson} name = {newName} namehandler = {handleNameChange} number = {newNumber} numberhandler = {handleNumberChange}/>
